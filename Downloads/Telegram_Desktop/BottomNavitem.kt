4edac26ec6ce8b66e25b2cea0f1f4a11c2529d37
Adilet2201 <149869637+Adilet2201@users.noreply.github.com>
2025-04-03 11:01:44 +0500

    Delete Downloads/Telegram_Desktop directory
    other files

--- Downloads/Telegram_Desktop/BottomNavitem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lazyrow.bottombar
-
-data class BottomNavItem(
-    val title: String,
-    val route: String,
-    val selectedIcon: androidx.compose.ui.graphics.vector.ImageVector,
-    val unselectedIcon: androidx.compose.ui.graphics.vector.ImageVector
-)
